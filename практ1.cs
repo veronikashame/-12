0b953ce487495fe85a419ea756349dfdc9f4a579
veronikashame <161477561+veronikashame@users.noreply.github.com>
2024-09-12 11:00:11 +0300

    Add files via upload

---
+++ практ1.cs
@@ -0,0 +1,43 @@
+﻿start:
+
+int a;
+int b;
+int c;
+
+char operation;
+
+
+Console.WriteLine("Введите первое число : ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nВыберите операцию:");
+Console.WriteLine("*");
+Console.WriteLine("/");
+Console.WriteLine("-");
+Console.WriteLine("+");
+operation = Convert.ToChar(Console.ReadLine());
+
+
+switch (operation)
+{
+    case '+':
+        c = a + b;
+        Console.WriteLine("Ответ " + (a) + " + " + (b) + " = " + (c).ToString());
+        goto start;
+    case '-':
+        c = a - b;
+        Console.WriteLine("Ответ " + (a) + " - " + (b) + " = " + (c).ToString());
+        goto start;
+    case '/':
+        c = a / b;
+        Console.WriteLine("Ответ " + (a) + " / " + (b) + "=" + (c).ToString());
+        goto start;
+    case '*':
+        c = a * b;
+        Console.WriteLine("Ответ " + (a) + " * " + (b) + " = " + (c).ToString());
+        goto start;
+    default:
+        Console.WriteLine("Ошибка оператора ");
+        goto start;
+}
